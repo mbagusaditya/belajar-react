eedb9075b57839613918186546130e6d407b0bca
mbagusaditya <bagusaditya9a19.dev@gmail.com>
2026-09-24 14:07:14 +0700

    fix: remove unused styles in App.css and re-design counter button

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,7 @@ function App() {
 
     return (
         <>
-            <section
-                id="center"
-                className="grid place-content-center bg-slate-900 h-screen"
-            >
+            <section className="grid place-content-center bg-slate-900 h-screen">
                 <div className="hero">
                     <img
                         src={heroImg}
@@ -28,125 +25,23 @@ function App() {
                     />
                     <img src={viteLogo} className="vite" alt="Vite logo" />
                 </div>
-                <div>
+
+                <div className="mb-3">
                     <h1 className="text-slate-100">Get started</h1>
                     <p className="text-slate-200">
                         Edit <code>src/App.tsx</code> and save to test{" "}
                         <code>HMR</code>
                     </p>
                 </div>
+
                 <button
                     type="button"
-                    className="bg-blue-700 text-slate-200 py-2 px-4 rounded-md active:ring-4 hover:bg-blue-800 ring-0 ring-blue-600/20 transition-all"
+                    className="border-blue-700 border-2 text-blue-500 py-2 px-4 rounded-md active:ring-4 hover:bg-blue-800 hover:text-slate-200 ring-0 ring-blue-600/20 transition-all w-fit mx-auto duration-200"
                     onClick={() => setCount((count) => count + 1)}
                 >
                     Count is {count}
                 </button>
             </section>
-
-            {/*<div className="ticks"></div>
-
-            <section id="next-steps">
-                <div id="docs">
-                    <svg
-                        className="icon"
-                        role="presentation"
-                        aria-hidden="true"
-                    >
-                        <use href="/icons.svg#documentation-icon"></use>
-                    </svg>
-                    <h2>Documentation</h2>
-                    <p>Your questions, answered</p>
-                    <ul>
-                        <li>
-                            <a href="https://vite.dev/" target="_blank">
-                                <img className="logo" src={viteLogo} alt="" />
-                                Explore Vite
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://react.dev/" target="_blank">
-                                <img
-                                    className="button-icon"
-                                    src={reactLogo}
-                                    alt=""
-                                />
-                                Learn more
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div id="social">
-                    <svg
-                        className="icon"
-                        role="presentation"
-                        aria-hidden="true"
-                    >
-                        <use href="/icons.svg#social-icon"></use>
-                    </svg>
-                    <h2>Connect with us</h2>
-                    <p>Join the Vite community</p>
-                    <ul>
-                        <li>
-                            <a
-                                href="https://github.com/vitejs/vite"
-                                target="_blank"
-                            >
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true"
-                                >
-                                    <use href="/icons.svg#github-icon"></use>
-                                </svg>
-                                GitHub
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://chat.vite.dev/" target="_blank">
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true"
-                                >
-                                    <use href="/icons.svg#discord-icon"></use>
-                                </svg>
-                                Discord
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://x.com/vite_js" target="_blank">
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true"
-                                >
-                                    <use href="/icons.svg#x-icon"></use>
-                                </svg>
-                                X.com
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://bsky.app/profile/vite.dev"
-                                target="_blank"
-                            >
-                                <svg
-                                    className="button-icon"
-                                    role="presentation"
-                                    aria-hidden="true"
-                                >
-                                    <use href="/icons.svg#bluesky-icon"></use>
-                                </svg>
-                                Bluesky
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </section>*/}
-
-            <div className="ticks"></div>
-            <section id="spacer"></section>
         </>
     );
 }
